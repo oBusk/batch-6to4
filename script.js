656bdc35d181c7a84c6d7555ec593bd7f88b4e58
Oscar Busk <oscar.busk@gmail.com>
2018-06-29 00:10:20 +0200

    Update script

--- script.js
+++ script.js
@@ -5,25 +5,35 @@ const path = require('path');
     const reg = /2002:(?:[A-F0-9]{1,4}:){2,6}([A-F0-9]{1,4}|:)/gmi
     const dirname = 'in';
 
+    const zeroPad = number => number <= 99 ? ("000" + number).slice(-2) : number;
+
     fs.readdir(dirname, (err, filenames) => {
         if (err) throw err;
 
+        let files = 0;
         filenames.forEach((filename) => {
             fs.readFile(path.join(dirname, filename), 'utf-8', (err, content) => {
                 if (err) throw err;
 
-                const replacedContent = content.replace(reg, (sub) => {
-                    const dataPart = sub.match(/2002:(.*)/)[1];
+                let replaces = 0;
+
+                const replacedContent = content.replace(reg, (input) => {
+                    const dataPart = input.match(/2002:(.*)/)[1];
                     const hextett = dataPart.split(':');
 
                     const h0 = parseInt(hextett[0], 16);
                     const h1 = parseInt(hextett[1], 16);
 
-                    return `${h0 >> 8}.${0x00ff & h0}.${h1 >> 8}.${0x00ff & h1}`;
+                    const output = `${h0 >> 8}.${0x00ff & h0}.${h1 >> 8}.${0x00ff & h1}`;
+
+                    console.info(`- (${zeroPad(++replaces)}) Replaced [${input}] → [${output}]`)
+
+                    return output;
                 });
 
                 fs.writeFile(path.join('out', filename), replacedContent, (err) => {
                     if (err) throw err;
+                    console.info(`[${zeroPad(++files)}] Outputting [${path.join('out', filename)}] (${replaces} replacements)`);
                 });
             });
         });
